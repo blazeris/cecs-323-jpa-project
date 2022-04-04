3a310538ca10027dfca824e23ecbe0a6f0b879c6
blazeris <blazeris@users.noreply.github.com>
2022-04-03 20:29:58 -0700

    book comments

--- src/main/java/csulb/cecs323/model/Books.java
+++ src/main/java/csulb/cecs323/model/Books.java
@@ -4,32 +4,52 @@ import javax.persistence.*;
 import java.util.Objects;
 import java.io.Serializable;
 
+/**
+ * A specific book that was authored and published, represents the writing not a specific physical copy.
+ */
 @Entity
 @Table(uniqueConstraints = {
         @UniqueConstraint(columnNames = {"title", "publisher"}),
         @UniqueConstraint(columnNames = {"title", "authoringEntity"})
 })
 public class Books implements Serializable {
+
+    // The ISBN code that identifies a specific book
     @Id
     @Column(nullable = false, length = 17)
     private String ISBN;
 
+    // The title of the book
     @Column(nullable = false, length = 80)
     private String title;
 
+    // The year that the book was published
     @Column(nullable = false)
     private int yearPublished;
 
+    // The author entity of the book
     @Column(nullable = false)
     private AuthoringEntity authoringEntity;
 
+    // The group that published the book
     @Column(nullable = false)
     private Publisher publisher;
 
+    /**
+     * Null constructor
+     */
     public Books(){
 
     }
 
+    /**
+     * Arguments constructor
+     * @param ISBN
+     * @param title
+     * @param yearPublished
+     * @param authoringEntity
+     * @param publisher
+     */
     public Books(String ISBN, String title, int yearPublished, AuthoringEntity authoringEntity, Publisher publisher){
         this.ISBN = ISBN;
         this.title = title;
@@ -78,6 +98,11 @@ public class Books implements Serializable {
         this.publisher = publisher;
     }
 
+    /**
+     * Function to check if ISBN is equal to object passed
+     * @param o Object which is being checked for equivalence
+     * @return Whether or not the object is equal
+     */
     @Override
     public boolean equals(Object o){
         boolean results = false;
@@ -93,11 +118,19 @@ public class Books implements Serializable {
         return results;
     }
 
+    /**
+     * Function to map ISBN, the primary key
+     * @return hash map
+     */
     @Override
     public int hashCode(){
         return Objects.hash(this.getISBN());
     }
 
+    /**
+     * String representation of a book
+     * @return
+     */
     @Override
     public String toString(){
         return ISBN;
