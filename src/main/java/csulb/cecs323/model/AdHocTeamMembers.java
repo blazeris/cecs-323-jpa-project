3eea017db9b95dbbb4bf1f41dc4dbd7403a041d8
Altaf Sayeed <altafsayeed19@yahoo.com>
2022-04-05 22:32:54 -0700

    javadoc stuff

--- src/main/java/csulb/cecs323/model/AdHocTeamMembers.java
+++ src/main/java/csulb/cecs323/model/AdHocTeamMembers.java
@@ -4,16 +4,13 @@ import java.util.Objects;
 @Entity
 @Table(name = "AD_HOC_TEAM_MEMBERS")
 public class AdHocTeamMembers {
-    //individual authors email
+    //name that uniquely identifies the member
     @Id
-    @ManyToOne
-    @JoinColumn(name= "INDIVIDUAL_AUTHORS_EMAIL")
-    private AuthoringEntities individualAuthorsEmail;
+    @Column(nullable=false, length = 30)
+    private String name;
     //email of the member
-    @Id
-    @ManyToOne
-    @JoinColumn(name ="AD_HOC_TEAMS_EMAIL")
-    private AuthoringEntities adHocTeamsEmail;
+    @Column(nullable = false, length = 50)
+    private String email;
 
     /**
      * Null constructor
@@ -23,25 +20,25 @@ public class AdHocTeamMembers {
     }
 /*
  *Arguments constructor
- *@param IndividualAuthorsEmail
- *@param AdHocTeamsEmail
+ *@param name
+ *@param email
  */
-    public AdHocTeamMembers(AuthoringEntities individualAuthorsEmail, AuthoringEntities adHocTeamsEmail){
-    this.individualAuthorsEmail = individualAuthorsEmail;
-    this.adHocTeamsEmail = adHocTeamsEmail;
+    public AdHocTeamMembers(String name, String email){
+    this.name = name;
+    this.email = email;
     }
 
-    public AuthoringEntities getIndividualAuthorsEmail(){
-        return individualAuthorsEmail;
+    public String getName(){
+        return name;
     }
-    public AuthoringEntities getAdHocTeamsEmail(){
-        return adHocTeamsEmail;
+    public String getEmail(){
+        return email;
     }
-    public void setIndividualAuthorsEmail(AuthoringEntities individualAuthorsEmail){
-        this.individualAuthorsEmail = individualAuthorsEmail;
+    public void setName(String name){
+        this.name=name;
     }
-    public void setAdHocTeamsEmail(AuthoringEntities adHocTeamsEmail){
-        this.adHocTeamsEmail=adHocTeamsEmail;
+    public void setEmail(String email){
+        this.email=email;
     }
     /**
      * toString function for members
@@ -49,24 +46,24 @@ public class AdHocTeamMembers {
      */
     @Override
     public String toString () {
-        return "Member's name " + this.individualAuthorsEmail + "\tEmail: " + this.adHocTeamsEmail;
+        return "Member's name " + this.name + "\tEmail: " + this.email;
     }
     /**
      * Function to check if name equals the object passed
      * @param o Object which is passed in to check if it equals
-     * @return the IndividualAuthorsEmail which equals the object passed
+     * @return the name which equals the object passed
      */
     @Override
     public boolean equals (Object o) {
         AdHocTeamMembers Members = (AdHocTeamMembers) o;
-        return this.getIndividualAuthorsEmail() == Members.getIndividualAuthorsEmail();
+        return this.getName() == Members.getName();
     }
     /**
-     * Hash function to map IndividualAuthorsEmail
+     * Hash function to map members name
      * @return hash map
      */
     @Override
     public int hashCode() {
-        return Objects.hash(this.getIndividualAuthorsEmail());
+        return Objects.hash(this.getName());
     }
 }
--- src/main/java/csulb/cecs323/model/AuthoringEntities.java
+++ src/main/java/csulb/cecs323/model/AuthoringEntities.java
@@ -17,13 +17,13 @@ import java.util.Objects;
 @Entity
 @Table(name = "AUTHORING_ENTITIES",
         uniqueConstraints = {
-            @UniqueConstraint(name = "authoring_entities_unique_01", columnNames = {"name", "email"})
-})
+                @UniqueConstraint(name = "authoring_entities_unique_01", columnNames = {"name", "email"})
+        })
 @NamedNativeQuery(
         name = "ReturnAuthoringEntities",
         query = "SELECT * " +
                 "FROM AUTHORING_ENTITIES " +
-        "WHERE AUTHORING_ENTITY_TYPE = ? ",
+                "WHERE AUTHORING_ENTITY_TYPE = ? ",
         resultClass = AuthoringEntities.class
 )
 @NamedNativeQuery(name = "ReturnAuthoringEntitiesPrimary",
@@ -48,18 +48,18 @@ public abstract class AuthoringEntities {
     @Column()
     private int yearFormed;
 
-   
+
     /**
-    * Null constructor
-    */
+     * Null constructor
+     */
     public AuthoringEntities() {
 
     }
 
     /**
      * Authoring Entity constructor
-     * @param email
-     * @param name
+     * @param email The email address of the authoring entity
+     * @param name The name of the authoring entity
      */
     public AuthoringEntities (String email, String name) {
         this.email = email;
@@ -70,18 +70,19 @@ public abstract class AuthoringEntities {
 
     /**
      * Writing group constructor
-     * @param email
-     * @param name
-     * @param headWriter
-     * @param yearFormed
+     * @param email The email address of the authoring entity
+     * @param name The name of the authoring entity
+     * @param headWriter The head writer of the authoring entity
+     * @param yearFormed The year the entity was formed
      */
     public AuthoringEntities (String email, String name,
-                      String headWriter, int yearFormed) {
+                              String headWriter, int yearFormed) {
         this.email = email;
         this.name = name;
         this.headWriter = headWriter;
         this.yearFormed = yearFormed;
     }
+
 
     public String getEmail() {
         return email;
@@ -116,11 +117,20 @@ public abstract class AuthoringEntities {
     }
 
 
+    /**
+     * toString function for email address
+     * @return email address of authoring entity
+     */
     @Override
     public String toString () {
         return email;
     }
 
+    /**
+     * Function to check if email is equal to object passed
+     * @param o Object being checked for equivalence
+     * @return Whether or not object is equal
+     */
     @Override
     public boolean equals(Object o){
         boolean results = false;
@@ -136,6 +146,10 @@ public abstract class AuthoringEntities {
         return results;
     }
 
+    /**
+     * Function to map email, the primary key
+     * @return hash map
+     */
     @Override
     public int hashCode() {
         return Objects.hash(this.getEmail());
